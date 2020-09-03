b932cda3ad1b6016287fae617f7d9cacc9e460c9
x0Yukthi <54282625+x0Yukthi@users.noreply.github.com>
2020-09-03 13:46:33 +0530

    Update monster.py

--- monster.py
+++ monster.py
@@ -8,18 +8,18 @@ bs = BeautifulSoup(wpages.content, 'html.parser')
 results = bs.find(id='ResultsContainer')
 job_elems = results.find_all('section', class_='card-content')
 for job_elem in job_elems:
-    title_elem = job_elem.find('h2', class_='title')
-    company_elem = job_elem.find('div', class_='company')
-    location_elem = job_elem.find('div', class_='location')
-    if None in (title_elem, company_elem, location_elem):
+    t_elem = job_elem.find('h2', class_='title')
+    c_elem = job_elem.find('div', class_='company')
+    l_elem = job_elem.find('div', class_='location')
+    if None in (t_elem, c_elem, l_elem):
         continue
-    print(title_elem.text)
-    print(company_elem.text)
-    print(location_elem.text)
+    print(t_elem.text)
+    print(c_elem.text)
+    print(l_elem.text)
     print()
-python_jobs = results.find_all('h2', string='Python Developer')
-python_jobs = results.find_all('h2',string=lambda text: 'python' in text.lower())
-for p_job in python_jobs:
+p_jobs = results.find_all('h2', string='Python Developer')
+p_jobs = results.find_all('h2',string=lambda text: 'python' in text.lower())
+for p_job in p_jobs:
     link = p_job.find('a')['href']
     print(p_job.text.strip())
     print(f"Apply here: {link}\n")
