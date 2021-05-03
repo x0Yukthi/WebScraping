6bef6e2e944cf8ef17d6bf72d1b7ab2debe4d009
Yukthi C <54282625+x0Yukthi@users.noreply.github.com>
2021-05-03 18:21:28 +0530

    Update scraping1.py

--- scraping1.py
+++ scraping1.py
@@ -5,23 +5,20 @@ url = requests.get(
     "https://www.flipkart.com/search?sid=tyy%2C4io&otracker=CLP_Filters&p%5B%5D=facets.brand%255B%255D%3DAPPLE").text
 soup = BeautifulSoup(url, 'lxml')
 # To get the text from web page
-result = soup.find_all('div', class_ ='_1YokD2 _2GoDe3')
+result = soup.find_all('div', class_='_3pLy-c row')
+print('\n    PHONE DETAILS')
 for res in result:
-    phn = res.find('div', class_ ='_3pLy-c row')
-    phn_name= res.find('div',class_='_4rR01T').text
-    phn_cong = res.find('div',class_='fMghEO').text
-    phn_price = res.find('div',class_='_30jeq3 _1_WHN1').text
-    actual_price = res.find('div',class_ = '_3I9_wc _27UcVY').text
-    emi = res.find('div',class_ ='_2ZdXDB').text
+    phn_name = res.find('div', class_='_4rR01T').text
+    phn_cong = res.find('div', class_='fMghEO').text
+    phn_price = res.find('div', class_='_30jeq3 _1_WHN1').text
+    actual_price = res.find('div', class_='_3I9_wc _27UcVY').text
+
     # To print the details
     print(f'''
-
-    PHONE DETAILS
 
     PHONE NAME --> {phn_name}
     CONFIGURATION --> {phn_cong}
     PRODUCT PRICE --> {actual_price}
     OFFER PRICE --> {phn_price}
-    OTHER DETAILS --> {emi}'
 
     ''')
